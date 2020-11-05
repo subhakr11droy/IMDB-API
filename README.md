313c69ad2728ddb03ef0df1789c11ea2f49e448e
Shubhakar Debroy <subhakr11droy@gmail.com>
2020-11-05 19:40:55 +0530

    BugFixes|Readme.md updated

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # IMDB-API
-A RESTful API for movies (something similar to IMDB)
+A RESTful API service created for movies (something similar to IMDB)
+
+# Requirements
+Following are the requirements:
+* Python version >= 3.8
+* Django 3.2
+* Also refer to requirements.txt
+
+#Installation
+1. clone desired directory with git clone https://github.com/divinedeveloper/imdb-task.git
+2. Create virtual environment
+3. pip3 install -r requirements.txt
+4. Navigate to folder
+5. Run migrations
+    * python3 manage.py makemigrations
+    * python3 manage.py migrate
+6. Populate data from JSON dump
+    * python3 manage.py load_data
+7. Run the server
+    *python3 manage.py runserver
+
+#Functionalities
+
+| Title | API Endpoints |
+| --- | --- |
+| View all Movies | /api/movies/ |
+| View All Genres | /api/genres/ |
+| Single movie detail | /api/movie/{id} |
+| Search by movie | /api/movies?search={movie-name} |
+| Search by genre | /api/movies?search={genre-name} |
+| Search by field | /api/movies?search={director-name} |
+| Search with multi-field | /api/movies?search={movie-name},{director-name},{genre-name},... |
+| Search with Pagination | /api/movies?search=<movie-name>&limit=<#> |
+
+#How to scale the application?
+Since, it completely depends on the scenario and requirements and a lot of factors are to be considered before coming up with an approach
+
+Following can be done to scale the application, looking at the current requirements
--- api/management/commands/load_data.py
+++ api/management/commands/load_data.py
@@ -1,36 +1,36 @@
 from django.core.management.base import BaseCommand
 from api.models import Movie, Genre
-from django.conf import settings
 import json
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         try:
-            json_file = f'{settings.BASE_DIR}/imdb_dump.json'
+            with open('imdb_dump.json') as file_obj:
+                list_of_movies = json.loads(file_obj.read())
 
-            with open(json_file) as file:
-                list_of_movies = json.loads(file.read())
+                # generator to generate all the movie from JSON string
                 movies_generator = (movie for movie in list_of_movies)
                 movie_obj, no_of_rows = {}, 0
 
+                # querying every movie in movie_generator and inserting in db
                 for movie in movies_generator:
                     movie_obj['popularity'] = movie.get('99popularity')
                     movie_obj['director'] = movie.get('director')
                     movie_obj['imdb_score'] = movie.get('imdb_score')
                     movie_obj['name'] = movie.get('name')
 
-                    movie_, created = Movie.objects.get_or_create(**movie_obj)
-                    genres = movie.get('genre')
+                    movie_, __ = Movie.objects.get_or_create(**movie_obj)
+                    genre_list = movie.get('genre')
 
                     # populate one or more genres for a movie
-                    for genre in genres:
-                        genre, created = Genre.objects.get_or_create(name=genre.strip())
-                        movie_.genre.add(genre)
+                    for genre in genre_list:
+                        genre_title, __ = Genre.objects.get_or_create(name=genre.strip())
+                        movie_.genre.add(genre_title)
                     movie_.save()
                     no_of_rows += 1
 
-                self.stdout.write(self.style.SUCCESS(f'Database populated!\n {no_of_rows} record(s) inserted'))
+                self.stdout.write(self.style.SUCCESS(f'Data loaded successfully!\n{no_of_rows} record(s) inserted'))
                 return
 
         except IOError as e:
