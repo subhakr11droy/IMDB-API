01fe381b22a96928618e20d6e22ef7cfad6a9bd1
Shubhakar Debroy <subhakr11droy@gmail.com>
2020-11-05 15:14:04 +0530

    Models and superuser created

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from api.models import Movie, Genre
+
 
 # Register your models here.
+# class MovieAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'director', 'popularity', 'imdb_score']
+#     list_filter = ['genre']
+#     search_fields = ['name', 'director']
+#
+#
+# class GenreAdmin(admin.ModelAdmin):
+#     search_fields = ['name']
+
+admin.site.register(Movie)
+admin.site.register(Genre)
+
+#
+# admin.site.register(Movie, MovieAdmin)
+# admin.site.register(Genre, GenreAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=10, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    popularity = models.FloatField()
+    director = models.CharField(max_length=50, db_index=True)
+    imdb_score = models.FloatField()
+    name = models.CharField(max_length=100, db_index=True)
+    genre = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        return self.name
