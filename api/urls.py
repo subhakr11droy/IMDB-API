13dae2957d2a922121d3d7607eae41b85c4e922f
Shubhakar Debroy <subhakr11droy@gmail.com>
2020-11-05 18:43:50 +0530

    apis for movies and genre done

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.api_overview, name="api-overview"),
-    path('movies/', views.movie_list, name="movie-list"),
-    path('genres/', views.genre_list, name="genre-list"),
+    # path('movies/', views.movie_list, name="movie-list"),
+    # path('genres/', views.genre_list, name="genre-list"),
+    path('movie/<str:id>', views.movie_details, name='movie-details'),
+    path('movies/', views.MovieListView.as_view(), name="movie-list"),
+    path('genres/', views.GenreListView.as_view(), name="genre-list"),
 ]
--- api/views.py
+++ api/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 from .serializers import MovieSerializer, GenreSerializer
 from .models import Movie, Genre
 
@@ -11,11 +15,15 @@ from .models import Movie, Genre
 @api_view(['GET'])
 def api_overview(request):
     api_urls = {
-        'List': '/movies/',
-        'List': '/genres/',
-        'Detail_view': '/task-list/',
-        'Detail_view1': '/task-list/',
-        'Detail_view2': '/task-list/',
+        'View All Movies': '/api/movies/',
+        'View All Genres': '/api/genres/',
+        'SingleMo detail': '/api/movie/<id>',
+        'Search by movie': '/api/movies?search=<movie-name>',
+        'Search by genre': '/api/movies?search=<genre-name>',
+        'Search by field': '/api/movies?search=<director-name>',
+        'Search with Pagination ': '/api/movies?search=<movie-name>&limit=<#>',
+        'Search with multi-field': '/api/movies?search=<movie-name>,<director-name>,<genre-name>,...',
+
     }
     return Response(api_urls)
 
@@ -32,3 +40,32 @@ def genre_list(request):
     genres = Genre.objects.all()
     serializer = GenreSerializer(genres, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def movie_details(request, id):
+    movies = Movie.objects.get(pk=id)
+    serializer = MovieSerializer(movies, many=False)
+    return Response(serializer.data)
+
+
+'''
+    Using default search filter makes all this CRUD operations trivial
+    API using search filter implementation
+'''
+
+
+class MovieListView(ListAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ['name', 'director', 'popularity', 'imdb_score', 'genre__name']
+
+
+class GenreListView(ListAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = [OrderingFilter]
+    search_fields = ['name']
